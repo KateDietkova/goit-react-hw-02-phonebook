294ee2408308994cf3a3a8b198d09973b3391a3e
Katerina Dietkova <katrinkir1296@gmail.com>
2022-12-02 11:36:00 +0200

    Add styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,70 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { nanoid } from 'nanoid';
+import { Box } from './Box/Box'; 
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addContact = contact => {
+    let isName = false;
+    const { contacts } = this.state;
+    contacts.map(({ name }) => {
+      if (contact.name === name) {
+        alert(`${contact.name} is already in contacts`);
+        isName = true;
+        return;
+      }
+    });
+    if (!isName) {
+      contact.id = nanoid();
+      this.setState(prevState => ({
+        contacts: [contact, ...prevState.contacts],
+      }));
+    }
+  };
+
+  onChangeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  visibleContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    const filterContacts = contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter)
+    );
+    return filterContacts;
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+    }));
+  }
+
+  render() {
+    const { filter } = this.state;
+    const changeFilter = this.onChangeFilter;
+    const filterContacts = this.visibleContacts();
+    const deleteContact = this.deleteContact;
+    return (
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <h1>Phonebook</h1>
+        <ContactForm addContact={this.addContact} />
+
+        <h2>Contacts</h2>
+        <Filter onSearch={changeFilter} value={filter} />
+        <ContactList
+          contacts={filterContacts}
+          onDeleteContact={deleteContact}
+        />
+      </Box>
+    );
+  }
+}
---
+++ src/components/Box/Box.jsx
@@ -0,0 +1,6 @@
+
+import styled from 'styled-components';
+import { space, layout, typography, color, flexbox } from 'styled-system';
+
+
+export const Box = styled('div')(space, layout, typography, color, flexbox);
